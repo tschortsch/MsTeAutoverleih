1ecfaa1a25db306ff1093929571933b878f1d8de
Juerg Hunziker <juerg.hunziker@gmail.com>
2011-12-04 10:56:22 +0100

    Startet implemented factory

---
+++ AutoReservation.Ui/Factory/LocalDataAccessCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoReservation.Common.Interfaces;
+using AutoReservation.Service.Wcf;
+
+namespace AutoReservation.Ui.Factory
+{
+    public class LocalDataAccessCreator : Creator
+    {
+        public override IAutoReservationService CreateBusinessLayerInstance()
+        {
+            // TODO return local service instance
+            return new AutoReservationService();
+        }
+    }
+}
---
+++ AutoReservation.Ui/Factory/RemoteDataAccessCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoReservation.Common.Interfaces;
+using AutoReservation.Service.Wcf;
+
+namespace AutoReservation.Ui.Factory
+{
+    public class RemoteDataAccessCreator : Creator
+    {
+        public override IAutoReservationService CreateBusinessLayerInstance()
+        {
+            return new AutoReservationService();
+        }
+    }
+}
