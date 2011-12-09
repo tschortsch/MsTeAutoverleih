7a9e0e4bbd1c0034b2d71be68116eec15ac6c5ff
Juerg Hunziker <juerg.hunziker@gmail.com>
2011-12-09 19:21:35 +0100

    implemented ServiceTests

--- AutoReservation.Testing/BusinessLayerTest.cs
+++ AutoReservation.Testing/BusinessLayerTest.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using AutoReservation.BusinessLayer;
-using AutoReservation.Dal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using AutoReservation.Service.Wcf;
+using AutoReservation.Common.DataTransferObjects;
 
 namespace AutoReservation.Testing
 {
@@ -14,48 +14,18 @@ namespace AutoReservation.Testing
         public void UpdateAutoTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-            AutoReservationBusinessComponent autoReservation = new AutoReservationBusinessComponent();
-            Auto originalAuto = autoReservation.Autos[0];
-            Auto modifiedAuto = autoReservation.Autos[0];
-            modifiedAuto.Marke = "Neue Marke";
-            autoReservation.UpdateAuto(modifiedAuto, originalAuto);
-            Assert.AreEqual(autoReservation.Autos[0].Marke, modifiedAuto.Marke);
-            //TODO: Add deep copy to have 2 truly different objects
-            //Assert.AreNotEqual(modifiedAuto.Marke, originalAuto.Marke);
-        }
-
-        [TestMethod]
-        public void GetAllAutosTest()
-        {
-            TestEnvironmentHelper.InitializeTestData();
-            AutoReservationBusinessComponent autoReservation = new AutoReservationBusinessComponent();
-            IList<Auto> autos = autoReservation.Autos;
-
-            IList<string> autoMarkenOriginal = new List<string>();
-            autoMarkenOriginal.Add("Fiat Punto");
-            autoMarkenOriginal.Add("VW Golf");
-            autoMarkenOriginal.Add("Audi S6");
-            IList<string> autoMarken = new List<string>();
-
-            foreach (Auto auto in autos)
-            {
-                autoMarken.Add(auto.Marke);
-            }
-            Assert.AreEqual(autoMarkenOriginal, autoMarken);
         }
 
         [TestMethod]
         public void UpdateKundeTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
         }
 
         [TestMethod]
         public void UpdateReservationTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
         }
 
     }
--- AutoReservation.Testing/ServiceTestBase.cs
+++ AutoReservation.Testing/ServiceTestBase.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AutoReservation.Common.Interfaces;
+using AutoReservation.Service.Wcf;
+using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
 
 namespace AutoReservation.Testing
 {
@@ -12,91 +15,155 @@ namespace AutoReservation.Testing
         public void AutosTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            IList<AutoDto> autos = service.Autos;
+            Assert.AreEqual(3, autos.Count);
         }
 
         [TestMethod]
         public void KundenTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            IList<KundeDto> kunden = service.Kunden;
+            Assert.AreEqual(4, kunden.Count);
         }
 
         [TestMethod]
         public void ReservationenTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            IList<ReservationDto> reservationen = service.Reservationen;
+            Assert.AreEqual(1, reservationen.Count);
         }
 
         [TestMethod]
         public void GetAutoByIdTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            IList<AutoDto> autos = service.Autos;
+            AutoDto first = autos[0];
+            int id = first.Id;
+            AutoDto autoById = service.GetAutoById(id);
+            Assert.AreEqual(first, autoById);
         }
 
         [TestMethod]
         public void GetKundeByIdTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            IList<KundeDto> kunden = service.Kunden;
+            KundeDto first = kunden[0];
+            int id = first.Id;
+            KundeDto kundeById = service.GetKundeById(id);
+            Assert.AreEqual(first, kundeById);
         }
 
         [TestMethod]
         public void GetReservationByNrTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            IList<ReservationDto> reservationen = service.Reservationen;
+            ReservationDto first = reservationen[0];
+            int nr = first.ReservationNr;
+            ReservationDto reservationById = service.GetReservationByNr(nr);
+            Assert.AreEqual(first, reservationById);
         }
 
         [TestMethod]
         public void GetReservationByIllegalNr()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            ReservationDto reservationById = service.GetReservationByNr(9999999);
+            Assert.IsNull(reservationById);
         }
 
         [TestMethod]
         public void InsertAutoTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            AutoDto auto = new AutoDto();
+            int count = service.Autos.Count;
+            int id = 99999;
+            auto.Id = id;
+            auto.Marke = "Marke";
+            service.InsertAuto(auto);
+            Assert.AreEqual(count + 1, service.Autos.Count);
         }
 
         [TestMethod]
         public void InsertKundeTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            int count = service.Kunden.Count;
+            KundeDto kunde = new KundeDto();
+            int id = 99999;
+            kunde.Id = id;
+            kunde.Nachname = "Nachname";
+            kunde.Vorname = "Vorname";
+            kunde.Geburtsdatum = System.DateTime.Today;
+            service.InsertKunde(kunde);
+            Assert.AreEqual(count + 1, service.Kunden.Count);
         }
 
         [TestMethod]
         public void InsertReservationTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            int count = service.Reservationen.Count;
+            ReservationDto reservation = new ReservationDto();
+            int id = 99999;
+            reservation.ReservationNr = id;
+            reservation.Kunde = service.Kunden[0];
+            reservation.Auto = service.Autos[0];
+            reservation.Von = System.DateTime.Today;
+            reservation.Bis = System.DateTime.Today;
+            service.InsertReservation(reservation);
+            Assert.AreEqual(count + 1, service.Reservationen.Count);
         }
 
         [TestMethod]
         public void UpdateAutoTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            AutoDto originalAuto = service.Autos[0];
+            AutoDto modifiedAuto = (AutoDto)originalAuto.Clone();
+            modifiedAuto.Marke = "Neue Marke";
+            service.UpdateAuto(modifiedAuto, originalAuto);
+            Assert.AreEqual(service.Autos[0], modifiedAuto);
         }
 
         [TestMethod]
         public void UpdateKundeTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            KundeDto originalKunde = service.Kunden[0];
+            KundeDto modifiedKunde = (KundeDto)originalKunde.Clone();
+            modifiedKunde.Nachname = "Neuer Nachname";
+            service.UpdateKunde(modifiedKunde, originalKunde);
+            Assert.AreEqual(service.Kunden[0], modifiedKunde);
         }
 
         [TestMethod]
         public void UpdateReservationTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            ReservationDto originalReservation = service.Reservationen[0];
+            ReservationDto modifiedReservation = (ReservationDto)originalReservation.Clone();
+            modifiedReservation.Von = System.DateTime.Today;
+            service.UpdateReservation(modifiedReservation, originalReservation);
+            Assert.AreEqual(service.Reservationen[0], modifiedReservation);
         }
 
         [TestMethod]
@@ -124,21 +191,39 @@ namespace AutoReservation.Testing
         public void DeleteKundeTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            int count = service.Kunden.Count;
+            KundeDto delete = service.Kunden[0];
+            int id = delete.Id;
+            service.DeleteKunde(delete);
+            Assert.AreEqual(count - 1, service.Kunden.Count);
+            Assert.IsNull(service.GetKundeById(id));
         }
 
         [TestMethod]
         public void DeleteAutoTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            int count = service.Autos.Count;
+            AutoDto delete = service.Autos[0];
+            int id = delete.Id;
+            service.DeleteAuto(delete);
+            Assert.AreEqual(count - 1, service.Autos.Count);
+            Assert.IsNull(service.GetAutoById(id));
         }
 
         [TestMethod]
         public void DeleteReservationTest()
         {
             TestEnvironmentHelper.InitializeTestData();
-
+            AutoReservationService service = new AutoReservationService();
+            int count = service.Reservationen.Count;
+            ReservationDto delete = service.Reservationen[0];
+            int nr = delete.ReservationNr;
+            service.DeleteReservation(delete);
+            Assert.AreEqual(count - 1, service.Reservationen.Count);
+            Assert.IsNull(service.GetReservationByNr(nr));
         }
     }
 }
